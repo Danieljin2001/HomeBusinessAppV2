0c208495f91243d2e6bb1e69b99126f84f57bf78
Daniel Jin <djin2001@gmail.com>
2021-11-25 00:39:07 -0700

    changed button pressable to touchableOpacity

--- HuslV1/src/screens/Home/index.js
+++ HuslV1/src/screens/Home/index.js
@@ -4,7 +4,8 @@ import {
     ImageBackground, 
     Text,
     Pressable,
-    
+    TouchableHighlight,
+    TouchableOpacity
  } from "react-native";
 import styles from "./styles";
 import Entypo from "react-native-vector-icons/Entypo";
@@ -18,20 +19,22 @@ const HomeScreen = (props) => {
     return (
         <View> 
             <View style={styles.column}>
-                <Pressable 
+                <TouchableOpacity 
                     style={styles.searchButton}
                     onPress={() => navigation.navigate("Search City")}
+                    activeOpacity={0.5}
                 >
                     <Entypo name="location-pin" size={20} color={"black"}/>
                     <Text style={styles.searchButtonText}>Locate your city</Text>
-                </Pressable>
+                </TouchableOpacity>
 
-                <Pressable
+                <TouchableOpacity
                     style={styles.findServiceButton}
                     onPress={() => navigation.navigate("Map Screen")}
+                    activeOpacity= {0.5}
                 >
                     <Text style= {styles.findServiceButtonText}>Find service</Text>
-                </Pressable>
+                </TouchableOpacity>
             </View>
             <ImageBackground 
                 source={require('../../../assets/images/wallpaper.png')} 
