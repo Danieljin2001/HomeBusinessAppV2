c12e42aa0b9f24614345e4bffdc0735c3a394d3f
Andrew Ma <hyung.joon2231621@gmail.com>
2021-12-15 22:28:05 -0500

    Google API implemented for Android and ios

--- HuslV1/src/screens/Explore/Map/index.js
+++ HuslV1/src/screens/Explore/Map/index.js
@@ -2,18 +2,24 @@ import React from "react";
 import { View, Text, Dimensions, SafeAreaView } from "react-native";
 import styles from './styles';
 import ActionSheet from "../../../components/ActionSheet";
+import MapView from 'react-native-maps';
 
 
-const {height} = Dimensions.get("window");
+// const {height} = Dimensions.get("window");
+const initRegion = {
+  latitude: 51.0447,
+  longitude: -114.0719,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+}
 
 const MapScreen = (props) => {
     return (
-        <View style = {{height}}>
-          <View style={styles.container}>
-            <Text> Map </Text>
-          </View>
-          <ActionSheet/>
-        </View>
+      <View style={styles.container}>
+        <MapView 
+          initialRegion={ initRegion }
+          style={styles.map} />
+      </View>
     );
 };
 
--- HuslV1/src/screens/Explore/Map/styles.js
+++ HuslV1/src/screens/Explore/Map/styles.js
@@ -1,11 +1,15 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
-    }
+    },
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
 });
 
 export default styles;
